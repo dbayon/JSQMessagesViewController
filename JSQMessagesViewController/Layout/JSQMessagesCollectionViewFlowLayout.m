24a628a2cc21b743c99bc93bd0549882465673a6
David Bayón <david.bayon@softwhisper.com>
2015-12-14 13:07:51 +0100

    no message

--- JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
+++ JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
@@ -470,7 +470,8 @@ const CGFloat kJSQMessagesCollectionViewAvatarSizeDefault = 30.0f;
         
         //  add extra 2 points of space, because `boundingRectWithSize:` is slightly off
         //  not sure why. magix. (shrug) if you know, submit a PR
-        CGFloat verticalInsets = verticalContainerInsets + verticalFrameInsets + 20.0f;
+        //CGFloat verticalInsets = verticalContainerInsets + verticalFrameInsets + 20.0f;
+        CGFloat verticalInsets = verticalContainerInsets + verticalFrameInsets + 2.0f;
         
         //  same as above, an extra 2 points of magix
         CGFloat finalWidth = MAX(stringSize.width + horizontalInsetsTotal, self.bubbleImageAssetWidth) + 2.0f;
